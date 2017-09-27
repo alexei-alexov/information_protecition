ce318c60f8760016f9906682f164b428bcd1fac7
Olexii Lukianchuk <lex@lex.fedora>
2017-09-27 13:06:42 +0300

    Done lab2 and lab3

--- ciphers/__init__.py
+++ ciphers/__init__.py
@@ -1,3 +1,4 @@
-from ciphers import *
-from playfair import Playfair
-from foursquare import Foursquare
+from ciphers.ciphers import *
+from ciphers.playfair import Playfair
+from ciphers.foursquare import Foursquare
+from ciphers.vigenere import Vigenere
